90fe99c3e8628f3981a5ce13a9e54e65230530f9
sam chris <vsgitsam25@gmail.com>
2025-11-09 09:42:24 +0300

    i actually doubt if i want to have anything to do with the sanity stuff, i would rather build my own backend

---
+++ app/client/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import "../globals.css";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import { ClerkProvider } from "@clerk/nextjs";
+
+export const metadata: Metadata = {
+  title: {
+    template: "%s - Shopcart online store",
+    default: "Shopcart online store",
+  },
+  description:
+    "Shopcart is an online store bringing the shopping experience to your fingertips",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider>
+      <div className="flex flex-col ">
+        <Header />
+        <main className="flex-1"> {children}</main>
+        <Footer />
+      </div>
+    </ClerkProvider>
+  );
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,11 @@
-import type { Metadata } from "next";
-import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import { ClerkProvider } from "@clerk/nextjs";
+import React from "react";
 
-export const metadata: Metadata = {
-  title: {
-    template: "%s - Shopcart online store",
-    default: "Shopcart online store",
-  },
-  description:
-    "Shopcart is an online store bringing the shopping experience to your fingertips",
+const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <html lang="en">
+      <body className="font-poppins antialiased">{children}</body>
+    </html>
+  );
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className="font-poppins antialiased">
-          <div className="flex flex-col ">
-            <Header />
-            <main className="flex-1"> {children}</main>
-            <Footer />
-          </div>
-        </body>
-      </html>
-    </ClerkProvider>
-  );
-}
+export default RootLayout;
